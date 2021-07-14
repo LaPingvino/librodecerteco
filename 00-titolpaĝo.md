2d8c69a8c5f387a6b0529f85460b783368e89068
Joop Kiefte <ikojba@gmail.com>
2021-07-14 11:48:52 +0000

    Simplify numbering

--- 00-titolpaĝo.md
+++ 00-titolpaĝo.md
@@ -4,10 +4,11 @@ author: Bahá'u'lláh
 documentclass: article
 papersize: a5
 ---
-# Libro de Certeco
 
 La Kitáb-i-Íqán de Bahá'u'lláh
 
 laŭ la angla traduko de SHOGHI EFFENDI
 
 esperantigis Lidia Zamenhof
+
+PROVLEGA VERSIO
--- make.sh
+++ make.sh
@@ -2,11 +2,11 @@
 (
 	for file in [0-9][0-9]*.md
 	do
-		sed -r 's/^([0-9]+)\. /\\lettrine[findent=3pt,nindent=0pt]{\1}/' $file
+		sed -r 's/^([0-9]+)\. /***\1.*** /' $file
 		echo
 		echo "\\pagebreak"
 		echo
 	done
 ) > final.md
 
-pandoc final.md --pdf-engine=xelatex --template=./template.tex -s -o kitabiiqan.pdf
+pandoc final.md --pdf-engine=xelatex -s -o kitabiiqan.pdf
